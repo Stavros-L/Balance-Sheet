494975a7e06166a3cd32dd2ea5e237de43471796
Stavros-L <stavros6@yandex.com>
2024-02-29 20:12:46 +0200

    fixed an issue that could crash the server

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,34 +2,25 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/UserSchema");
 
 const isAuthenticated = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1]; // Assuming JWT token is included in the Authorization header
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
   try {
-    // Verify token
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decodedToken.userId;
-
-    // Fetch user document from the database
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
+    // Check if Authorization header exists
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Authorization header missing" });
     }
 
-    // Attach user information to the request object
-    req.user = {
-      userId: user._id,
-      username: user.username,
-      salary: user.salary,
-      monthlyBudget: user.monthlyBudget
-    };
+    // Split the Authorization header and retrieve the token
+    const token = req.headers.authorization.split(" ")[1]; 
+
+    // Verify token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach userId to request object for further middleware/routes
+    req.user = { userId: decodedToken.userId };
 
     next();
   } catch (error) {
     console.error(error);
-    res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
